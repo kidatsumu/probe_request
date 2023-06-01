764f2d40eebf512d5e87f55767a675a3af477e0c
kida <kida@cysec.cs.ritsumei.ac.jp>
2023-06-01 16:00:59 +0900

    add: 先頭がMACアドレスでその後にSSIDが複数続く配列を作る

--- probe_request/prdb.py
+++ probe_request/prdb.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import re
 
 data = pd.read_csv("/Users/kidago/Documents/probe_request/2023_4_19_osaka_shinosaka_w.csv",header=0)
@@ -17,7 +18,32 @@ print(data)
 #group1.apply(print)
 data["mac_num"] = data["Source"].factorize()[0]
 data["SSID_num"] = data["Info"].factorize()[0]
-print(data)
+list1 = np.zeros((100,2))
+tmp = 0
+p = 0
+#先頭がMACアドレスでその後にSSIDが複数続く配列を作る
+while True:
+    if tmp == data.shape[0]:
+        break
+
+    tmp2 = 0
+
+    for t in list1:
+        if list1[tmp2][0] == data.loc[tmp,"Source"]:
+            print("b")
+            list1[list1.index(data.loc[tmp,"Source"])].append(data.loc[tmp,"Info"])
+            break
+        elif all(list1[tmp2]):
+            print("b")
+            list1[p] = [data.loc[tmp,"Source"],data.loc[tmp,"Info"]]
+            p = p + 1
+            break
+        
+        tmp2 = tmp2 + 1
+            
+    tmp = tmp + 1
+
+print(list1)
 #シーケンス番号で連続しているものでグループ分け
 
 #グループ分けしたものをSSIDの組み合わせの一致割合の高いものでグループ分け
